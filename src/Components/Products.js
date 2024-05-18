98a7cdd1faa2affb0e2f8c5fb1d29cb1b5fca5ae
“Ritammishra”~ <mishraritam935@gmail.com>
2024-05-18 12:29:06 +0530

    update category

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { add } from '../store/cartSlice';
 
-
 const Products = () => {
 
     const dispatch = useDispatch()
@@ -14,22 +13,78 @@ const Products = () => {
             .then((res) => {
                 const items = res.data
                 setProducts(items)
-                console.log(items);
+                // console.log(items);
             })
-            .catch((err) => {   
+            .catch((err) => {
                 console.log(err);
             })
     }, [])
 
     const handleAdd = (products) => {
         dispatch(add(products))
+
+    }
+
+    const electronics = () => {
+        axios.get('https://fakestoreapi.com/products/category/electronics')
+        .then((res) => {
+            const items = res.data
+            console.log(items);
+            setProducts(items)
+        })
+        .catch((err)=>console.log(err))
+    }
+    const allproduct = () => {
+        axios.get('https://fakestoreapi.com/products')
+        .then((res) => {
+            const items = res.data
+            console.log(items);
+            setProducts(items)
+        })
+        .catch((err)=>console.log(err))
+    }
+
+    const jewelery = () => {
+        axios.get('https://fakestoreapi.com/products/category/jewelery')
+        .then((res) => {
+            const items = res.data
+            console.log(items);
+            setProducts(items)
+        })
+        .catch((err)=>console.log(err))
+    }
+
+    const mens = () => {
+        axios.get("https://fakestoreapi.com/products/category/men's%20clothing")
+        .then((res) => {
+            const items = res.data
+            console.log(items);
+            setProducts(items)
+        })
+        .catch((err)=>console.log(err))
+    }
+    const women = () => {
+        axios.get("https://fakestoreapi.com/products/category/women's%20clothing")
+        .then((res) => {
+            const items = res.data
+            console.log(items);
+            setProducts(items)
+        })
+        .catch((err)=>console.log(err))
     }
 
     return (
-    <>
-    <div className="flex" style={{ display: "flex", flexWrap: "wrap", backgroundColor: "#0E2A47", justifyContent: "center" }}>
+        <>
+            <div className="flex h-{100vh}" style={{ display: "flex", flexWrap: "wrap", backgroundColor: "#0E2A47", justifyContent: "center" }}>
+                <div className='w-2/4 h-8 bg-white mt-4 flex justify-around items-center rounded'>
+                <p className='mr-2 cursor-pointer' onClick={allproduct}>All Product</p>
+                   <p className='mr-2 cursor-pointer' onClick={electronics}>Electronics</p>
+                   <p className='mr-2 cursor-pointer' onClick={jewelery}>Jewelery</p>
+                   <p className='mr-2 cursor-pointer' onClick={mens}>Mens</p>
+                   <p className='mr-2 cursor-pointer' onClick={women}>Women</p>
+                </div>
                 <div className="flex flex-wrap justify-center">
-                {
+                    {
                         products.map((item, index) => (
                             <div key={index} className=" w-80 p-4 m-2 bg-white rounded-lg shadow-md transform hover:scale-105 transition-transform duration-300 ease-in-out mt-4">
                                 <img className="w-full h-40 object-cover rounded-t-lg" alt="Card Image" src={item.image} />
@@ -38,7 +93,7 @@ const Products = () => {
                                     <p className="truncate">{item.description}</p>
                                     <p className="truncate text-xl">${item.price}</p>
                                     <div className="flex justify-between items-center mt-4">
-                                        <button onClick={()=> handleAdd(item)} className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-400">Add Cart</button>
+                                        <button onClick={() => handleAdd(item)} className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-400">Add Cart</button>
                                     </div>
                                 </div>
                             </div>
@@ -46,7 +101,7 @@ const Products = () => {
                     }
                 </div>
             </div>
-    </>
+        </>
     )
 }
 
